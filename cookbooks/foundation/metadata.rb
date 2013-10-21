eb8985a878eebe666250e7ed4fff16fb26399bd9
Trevor Bramwell <trevor@bramwell.net>
2013-10-21 10:12:53 -0700

    Foundation: initial commit

---
+++ cookbooks/foundation/metadata.rb
@@ -0,0 +1,7 @@
+name             'foundation'
+maintainer       'Trevor Bramwell'
+maintainer_email 'trevor@bramwell.net'
+license          'Apache 2.0'
+description      'Installs/Configures foundation'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
